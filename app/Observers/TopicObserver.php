0518e99e115de5e0f0aa1b31c51ee0d17e4cdf1b
chenyang <1194884851@qq.com>
2021-10-30 21:00:04 +0800

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,10 @@ class TopicObserver
     // 在 Topic 模型中保存时触发的 saving 事件中, 对 except 字段进行赋值
     public function saving(Topic $topic)
     {
+        // 用户输入的内容进行过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 对用户内容进行摘要操作
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
