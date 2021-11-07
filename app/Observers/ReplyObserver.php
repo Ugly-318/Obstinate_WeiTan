21476618f2bbf3292a6ed186e3eaa5465fe28ca6
chenyang <1194884851@qq.com>
2021-11-07 16:00:50 +0800

    用户发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,25 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    // 对用户评论内容过滤
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
+
+        // 判断内容为空的处理方式 拒绝保存入库
+        if ($reply->content === '') {
+            return false;
+        }
     }
 
-    public function updating(Reply $reply)
+    //
+    public function created(Reply $reply)
     {
-        //
+        // 话题下没新增一条回复 进行 +1
+        // $reply->topic->increment('reply_count', 1);
+
+        // 创建成功后计算本话题下评论总数 对 `reply_count` 字段进行赋值
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 }
