4af2fc67fa111a7b344dc267ceabdcfa21ec1df2
chenyang <1194884851@qq.com>
2021-11-16 17:04:22 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -29,5 +30,8 @@ class ReplyObserver
         // 创建成功后计算本话题下评论总数 对 `reply_count` 字段进行赋值
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
